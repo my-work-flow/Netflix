2d54a447968b5489a84627840dc1aee2f99b9d7f
my-work-flow <zuriyashyasin24@gmail.com>
2024-12-28 15:36:39 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Netflix</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
